e4036520da429873d0b9e302c08bb6f2b178c72e
Kevin Timmons <khouse81@gmail.com>
2019-10-17 13:12:13 -0700

    added comments including author name

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -2,6 +2,13 @@ package edu.pcc.cis.helloworld;
 
 import java.util.Date;
 
+/**
+ * HelloWorld java program for exercises to learn git as hosting site with
+ * local repository and intelliJ
+ *
+ * @author  Kevin Timmons
+ * @version 2019.10.17
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
